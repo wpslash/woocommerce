32b88bb701898573da914558fc246228ff08ce40
wpslash <57621710+wpslash@users.noreply.github.com>
2019-11-11 13:01:15 +0200

    Update cart-item-data.php
    echo colon  only if key value exists...

--- templates/cart/cart-item-data.php
+++ templates/cart/cart-item-data.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <dl class="variation">
 	<?php foreach ( $item_data as $data ) : ?>
-		<dt class="<?php echo sanitize_html_class( 'variation-' . $data['key'] ); ?>"><?php echo wp_kses_post( $data['key'] ); ?>:</dt>
+		<dt class="<?php echo sanitize_html_class( 'variation-' . $data['key'] ); ?>"><?php echo wp_kses_post( $data['key'] ); ?><?php if($data['display'] && $data['key'] ) echo ":"; ?></dt>
 		<dd class="<?php echo sanitize_html_class( 'variation-' . $data['key'] ); ?>"><?php echo wp_kses_post( wpautop( $data['display'] ) ); ?></dd>
 	<?php endforeach; ?>
 </dl>
